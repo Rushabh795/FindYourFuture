1e54b4b97584f2caf4c65a52820b16e7a878cd7b
rushabhshah <rushabh795@gmail.com>
2022-11-11 12:17:58 -0500

    Update UI and Logic
    - Change design for login page
    - Added Image
    - Add logic ( click event ) into  LoginPage.xml.cs
    -Added Validation

--- FindYourFuture/FindYourFuture/LoginPage.xaml.cs
+++ FindYourFuture/FindYourFuture/LoginPage.xaml.cs
@@ -6,4 +6,16 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    async private void OnButtonClicked(object sender, EventArgs e)
+    {
+        if (String.IsNullOrWhiteSpace(usernameEntry.Text) || String.IsNullOrWhiteSpace(passwordEntry.Text))
+        {
+            await DisplayAlert("Error", "Please fill all the fields", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Welcome", $"Hello, {usernameEntry.Text}", "OK");
+        }
+    }
 }
+
