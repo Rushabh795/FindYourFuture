11b0bb0a9918da6b0b8270dd571947a9a0382634
rushabhshah <rushabh795@gmail.com>
2022-11-11 11:28:09 -0500

    #1 Project Created
    - Created MAUI Project for an application.
    -MAUI has support of Android , IOS and Web so it is multiplatform Application.

---
+++ FindYourFuture/FindYourFuture/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FindYourFuture;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ FindYourFuture/FindYourFuture/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FindYourFuture;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
